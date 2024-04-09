4dd7467baaee77e81d417544f1068ede4438886e
StopSoo <jpy1030@naver.com>
2024-04-09 17:18:43 +0900

    <feat>: 폰트 적용 및 기본 폰트 구성

---
+++ lib/components/textStyles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const TextStyle headerTextStyle = TextStyle(
+  fontFamily: 'NotoSans',
+  color: CupertinoColors.black,
+  fontWeight: FontWeight.w700,
+  fontSize: 14,
+);
+
+const TextStyle defaultTextStyle = TextStyle(
+  fontFamily: 'Inter',
+  color: CupertinoColors.black,
+  fontWeight: FontWeight.w500,
+);
+
