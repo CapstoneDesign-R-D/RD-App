ea1f3d3247c46545de0da754ef0b42c2b5488ea1
StopSoo <jpy1030@naver.com>
2024-06-04 01:46:41 +0900

    <feat> 제거 기록 페이지에 api 연결 시도

--- lib/screen/record_of_removal_screen.dart
+++ lib/screen/record_of_removal_screen.dart
@@ -1,6 +1,8 @@
 /*로봇 맵 띄울 화면*/
 import 'package:flutter/material.dart';
 import 'package:flutter_app/widget/bar.dart';
+import 'package:flutter_app/service/api_service.dart';
+import 'package:flutter_app/model/object_check.dart';
 
 class RecordOfRemovalScreen extends StatefulWidget {
   const RecordOfRemovalScreen({ super.key });
@@ -10,6 +12,25 @@ class RecordOfRemovalScreen extends StatefulWidget {
 }
 
 class _RecordOfRemovalScreenState extends State<RecordOfRemovalScreen> {
+  final ApiService _apiService = ApiService();
+  List<ObjectCheck> _objectCheckList = [];  // 객체 인식 정보 리스트 
+  
+  @override
+  void initState() {
+    super.initState();
+    _fetchObjectCheckList();
+  }
+
+  Future<void> _fetchObjectCheckList() async {
+    try {
+      List<ObjectCheck> objectCheckList = await _apiService.fetchObjectCheckList();
+      setState(() {
+        _objectCheckList = objectCheckList;
+      });
+    } catch (e) {
+      print('객체 인식에 실패하였습니다. : $e');
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,10 +42,32 @@ class _RecordOfRemovalScreenState extends State<RecordOfRemovalScreen> {
           child: Column(
             children: [
               Bar(), // 상단 제어 바
-              
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _objectCheckList.length,
+                  itemBuilder: (context, index) {
+                    return _buildObjectCheckItem(_objectCheckList[index]);
+                  },
+                )
+              )
             ],
           )
         ),
+      ),
+    );
+  }
+
+  Widget _buildObjectCheckItem(ObjectCheck objectCheck) {
+    return ListTile(
+      title: Text(objectCheck.location ?? '인식하지 못 한 위치'),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Class: ${objectCheck.detectedClass ?? '인식하지 못 한 클래스'}'),
+          Text('Date: ${objectCheck.detectedDate ?? '인식하지 못 한 날짜'}'),
+          Text('Time: ${objectCheck.detectedTime?.hour}:${objectCheck.detectedTime?.minute}:${objectCheck.detectedTime?.second}'),
+          Text('Check: ${objectCheck.detectionCheck == true ? 'Yes' : 'No'}'),
+        ],
       ),
     );
   }
--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,21 +1,29 @@
+import 'dart:convert'; // for json decoding
 import 'package:dio/dio.dart';
 import '../model/object_check.dart';
 
 class ApiService {
   final Dio _dio = Dio();
 
-  Future<ObjectCheck> fetchObjectCheck() async {
+  Future<List<ObjectCheck>> fetchObjectCheckList() async {
     try {
-      Response response = await _dio.get('http://3.34.109.184:8080/api/object/check');
+      Response response = await _dio.get(
+        'http://3.34.109.184:8080/api/object/check',
+        options: Options(
+          responseType: ResponseType.plain,  // json으로 응답 받기
+        )
+      );
 
       if (response.statusCode == 200) { // success
-        return ObjectCheck.fromJson(response.data);
+        print('Response data: ${response.data}');
+        List<ObjectCheck> objectCheckList = List<ObjectCheck>.from(jsonDecode(response.data).map((x) => ObjectCheck.fromJson(x)));
+        return objectCheckList;
       } else {
-        throw Exception('객체 인식 정보를 불러오는 데 실패하였습니다.');
+        throw Exception('객체 인식 정보 리스트1을 불러오는 데 실패하였습니다.');
       }
     } catch (e) {
-      print(e);
-      throw Exception('객체 인식 정보를 불러오는 데 실패하였습니다.');
+      print('Error: $e');
+      throw Exception('객체 인식 정보 리스트2를 불러오는 데 실패하였습니다.');
     }
   }
 }
